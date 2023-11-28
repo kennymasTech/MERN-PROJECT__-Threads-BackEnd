6094fca6c77172a2a98da08831dea92d39efee01
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-28 16:48:46 +0100

    a

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 
 const express = require ('express');
-const dotenv = require ("dotenv")
+const dotenv = require ("dotenv");
 const cookieParser = require ("cookie-parser");
 const mongoose = require ("mongoose");
 const userRoutes = require ("./routes/userRoutes")
