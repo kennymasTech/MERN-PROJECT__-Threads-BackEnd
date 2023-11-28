e08cde415e34f038e0d1fbdbd0fab4051520c20c
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-28 16:46:52 +0100

    a

--- controllers/userController.js
+++ controllers/userController.js
@@ -36,7 +36,7 @@ const signUpUser = async (req, res) => {
     } catch (error) {
             res.status(500).json({ message: error.message });
             console.log("Error in signupUser: ", error.message)
-        }
+        };
 
 };
 
