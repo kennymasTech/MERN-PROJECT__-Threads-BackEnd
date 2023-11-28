a2df75f996e9bcc08caebd75d898cb5cd189a22d
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-28 16:44:55 +0100

    a

--- utils/helper/generateTokenAndSetCookies.js
+++ utils/helper/generateTokenAndSetCookies.js
@@ -1,5 +1,5 @@
 
-const jwt = require ("jsonwebtoken")
+const jwt = require ("jsonwebtoken");
 
 const generateTokenAndSetCookie = (userId, res) => {
     const token = jwt.sign({userId},process.env.JWT_SECRET, {
