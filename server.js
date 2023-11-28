b500812e0836e99050a201ca25c1f5d135347478
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-28 16:49:49 +0100

    a

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json({limit : "50mb"}))  // Parse json data inside the req body;
 app.use(express.urlencoded({extended: true}))  // Parse form data inside the req body;
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use("/api/users", userRoutes)
 
