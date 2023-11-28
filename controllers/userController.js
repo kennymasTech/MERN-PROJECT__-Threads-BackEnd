eb3feb245ec2e82dd9955f0c9e1f07b174874b54
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-28 16:09:55 +0100

    a

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,27 +1,23 @@
-const User = require ("../models/userModel")
-const bcrypt = require("bcryptjs")
+const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
 
-const signUpUser = async(req, res) => {
+const signUpUser = async (req, res) => {
     try {
-        const {name, email, username, password} = req.body;
-        const user = await User.findOne({$or:[{email},{username}]})
-
-        if (user) {
-            return res.status(400).json({message: "User Already Exists"})
+        const {name, email, password, username} = req.body;
+        const user = await User.findOne({ $or:[ {email}, {username} ]});
+        
+        if(user) {
+            return res.status(400).json({message: "User already exists" });
         }
 
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(password, salt)
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
         const newUser = new User({
-            name,
-            email,
-            username,
-            password: hashedPassword,
+            name, email, username, password: hashedPassword
         })
 
-        await newUser.save()
-
+        await newUser.save();
         if(newUser) {
             res.status(201).json({
                 _id: newUser._id,
@@ -29,15 +25,16 @@ const signUpUser = async(req, res) => {
                 email: newUser.email,
                 username: newUser.username,
             })
-        } else {
-            res.status(404).json({message: "Invalid User Data"})
+        } else { 
+            res.status(400).json({ message: "Invalid user data" });
+        }
+    } catch (error) {
+            res.status(500).json({ message: error.message });
+            console.log("Error in signupUser: ", error.message)
         }
 
+    
+	
+};
 
-    } catch (error) {
-        res.status(500).json({message: error.message})
-        console.log("Error In SignUpUser:", error.message);
-    }
-}
-
-module.exports = { signUpUser }
+module.exports = { signUpUser };
