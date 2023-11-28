3358198e485bf7e79f7e16585af04a55acaa9307
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-28 09:16:06 +0100

    a

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
 
 dotenv.config()
+const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json({limit : "50mb"}))  // Parse json data inside the req body;
 app.use(express.urlencoded({extended: true}))  // Parse form data inside the req body;
 app.use(cookieParser())
+
+
+app.get("/", (req,res) => {
+    res.send("Welcome Home 🏠")
+})
