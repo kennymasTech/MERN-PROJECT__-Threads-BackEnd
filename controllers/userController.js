ef0f3d1152fcc39ee85d8495ffefeca485614630
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-29 11:23:20 +0100

    a

--- controllers/userController.js
+++ controllers/userController.js
@@ -71,7 +71,7 @@ const loginUser = async (req, res) => {
 
 const logOutUser = (req, res) => {
     try {
-        
+        res.cookie("jwt", "", {maxAge: 1})
     } catch (error) {
         
     }
