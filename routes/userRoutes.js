32a1383bfc1dc3f3fb1bd648c2c72187fb60e472
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-29 11:52:49 +0100

    a

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,6 @@ const router = express.Router();
 router.post("/signup", signUpUser);
 router.post("/login", loginUser);
 router.post("/logOut", logOutUser);
-router.post("/follow/:id", protectRoute, followUnFollowUser);  // Toggle State( Follow/Unfollow)
+router.post("/follow/:id",  followUnFollowUser);  // Toggle State( Follow/Unfollow)
 
 module.exports = router;
