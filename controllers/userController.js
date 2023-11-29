a61798c7ca90f9fe7dba06adb68580f5e236ed9a
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-29 09:59:48 +0100

    a

--- controllers/userController.js
+++ controllers/userController.js
@@ -42,7 +42,7 @@ const signUpUser = async (req, res) => {
 const loginUser = async (req, res) => {
     try {
         const { username, password } = req.body;
-        const user = await user.findOne({ username: username})
+        const user = await user.findOne({ username: username })
     } catch (error) {
         
     }
