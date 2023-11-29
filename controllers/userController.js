65c613feffa429bc9da47f3156efdf70719f8dab
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-29 12:13:29 +0100

    m

--- controllers/userController.js
+++ controllers/userController.js
@@ -95,6 +95,12 @@ const followUnFollowUser = async (req, res) => {
         if(!isFollowing) {
 
             // Unfollow user
+            await User.findByIdAndUpdate(id, {$pull: {followers: req.user._id}})
+            await User.findByIdAndUpdate(req.user._id, {$pull: {following: id}})
+            res.status(200).json({ meaasage: "User Unfollowed Successfully"})
+
+        } else {
+            // Follow User
             
         }
         
