d0b0ea3a0131f8e258a9b6871a4e60151b4621bd
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-30 11:44:30 +0100

    a

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { createPost } = require('../controllers/postControllers');
+const protectRoute = require('../middleware/protectRoute');
 
 const router = express.Router();
 
-router.post("/create", createPost)
+router.post("/create", createPost, protectRoute)
 
 module.exports = router;
