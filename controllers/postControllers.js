abc4cd55a20de79951b301b58de33580b56888d8
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-30 10:54:20 +0100

    a

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -1,8 +1,18 @@
-const { createPost } = require('../controllers/postControllers');
-
+const User = require("../models/userModel");
 
 const createPost = async (req, res) => {
+    try {
+        const { postedBy, text, img } = req.body;
 
+        if( !postedBy || !text ) {
+            return res.status(400).json({ message: "PostedBy And Text Field Are Required" })
+        }
+
+        const user = await User.findById(postedBy);
+        
+    } catch (error) {
+        
+    }
 }
 
 module.exports = { createPost }
