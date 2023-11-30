83ce59214b9c27efcc35124132e0310292a75d2a
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-30 12:06:23 +0100

    a

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -35,8 +35,14 @@ const createPost = async (req, res) => {
 
 const getPost = async (req, res) => {
     try {
-        const posts = await Post.find();
-        res.status(200).json(posts)
+        const posts = await Post.findById(req.params.id);
+
+        if ( !post ) {
+            return res.status(404).json({ message: "Post Not Found" })
+        }
+
+        res.status(200).json({posts})
+
     } catch (error) {
         res.status(500).json({ message: error.message });  //  Internal Server Error
         console.log("Error In GetPost: ", error.message);
