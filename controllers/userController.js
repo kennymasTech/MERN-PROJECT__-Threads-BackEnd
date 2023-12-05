70feccebf5a1e0a3506d88e0e7b27088dc4004e9
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-12-05 15:44:33 +0100

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -60,6 +60,8 @@ const signUpUser = async (req, res) => {
         name: newUser.name,
         email: newUser.email,
         username: newUser.username,
+        bio: newUser.bio,
+        profilePic: newUser.profilePic,
       });
     } else {
       res.status(400).json({ error: "Invalid User Data" });
