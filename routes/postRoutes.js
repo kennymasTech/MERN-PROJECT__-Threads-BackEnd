ee958bb7ea0c86ca0272a375e209b05abc637354
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-12-11 10:56:14 +0100

    a

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -17,7 +17,8 @@ router.get("/feed", protectRoute, getFeedPost);
 router.get("/:id", getPost);
 router.post("/create", protectRoute, createPost);
 router.delete("/:id", protectRoute, deletePost);
-router.put("/like/:id", protectRoute, likeUnlikePost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/reply/:id", protectRoute, replyToPost);
 router.post("/reply/:id", protectRoute, replyToPost);
 router.post("/user/:username", getUserPosts);
 
