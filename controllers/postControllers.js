20dccfa07799408de99ff5eb7997de2db2fce6f0
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-12-12 11:08:27 +0100

    a

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -52,7 +52,7 @@ const getPost = async (req, res) => {
       return res.status(404).json({ message: "Post Not Found" });
     }
 
-    res.status(200).json( post );
+    res.status(200).json(post );
   } catch (error) {
     res.status(500).json({ message: error.message }); //  Internal Server Error
     console.log("Error In GetPost: ", error.message);
